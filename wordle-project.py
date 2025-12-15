ca554d3dad668c58256cf9a3530e0a35988ccbbd
Ryan Craig <Ryan@MacBook-Air-4.local>
2025-12-14 19:45:23 -0600

    Enchanged Wordle: file reading, any word length, ANSI colors, one-line display

--- wordle-project.py
+++ wordle-project.py
@@ -1,25 +1,30 @@
 import random
 
 def main():
-    words = ["SIXTY", "SEVEN", "BALLS", "BUHHH", "SHAPT", "HOLLY", "WORDL", "LOSER", "LMFAO", "RYANN", "SAMII", "SPENC"]
+    file = open("words.txt", "r")
+    content = file.read()
+    words = content.split('\n')
+    file.close()
     word_index = random.randint(0, len(words)-1)
     target_word = words[word_index]
     correct = False
     for i in range(6):
         print(f"You are on attempt {i + 1}")
+        results = ""
         while True:
             guess = (input("Word: ").upper())
-            if len(guess) != 5:
-                print("Failure. Try again with a viable 5 letter word. Try again.")
+            if len(guess) != len(target_word):
+                print(f"Failure. Try again with a viable {len(target_word)} letter word. Try again.")
             else:
                 break
-        for j in range(5):
+        for j in range(len(target_word)):
             if guess[j] == target_word[j]:
-                print(f"Same letter, Same position: {guess[j]}")
+                results = results + f"\033[32m[{guess[j]}]\033[0m"
             elif guess[j] in target_word:
-                print(f"Same letter, Wrong position: {guess[j]}")
+                results = results + f"\033[33m[{guess[j]}]\033[0m"
             else:
-                print(f"Not in word at all: {guess[j]}")
+                results = results + (f"\033[90m[{guess[j]}]\033[0m")
+        print(results)
         if guess == target_word:
             correct = True
             print("Good job!")
@@ -28,4 +33,3 @@ def main():
         print(f"You lose! Correct word was {target_word}")
 
 main()
-
